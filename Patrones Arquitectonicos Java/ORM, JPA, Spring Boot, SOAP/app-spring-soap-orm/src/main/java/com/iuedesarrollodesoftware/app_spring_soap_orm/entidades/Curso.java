de85bf322e95ef9c9f31b30abb4c9465a7b56758
JUAN PABLO RESTREPO JIMENEZ <jprestrepo346@gmail.com>
2024-09-18 17:01:00 -0500

    investigue sobre soap e inicialize una aplicacion de ejemplo

---
+++ Patrones Arquitectonicos Java/ORM, JPA, Spring Boot, SOAP/app-spring-soap-orm/src/main/java/com/iuedesarrollodesoftware/app_spring_soap_orm/entidades/Curso.java
@@ -0,0 +1,7 @@
+package com.iuedesarrollodesoftware.app_spring_soap_orm.entidades;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Curso {
+  }
---
+++ Patrones Arquitectonicos Java/ORM, JPA, Spring Boot/orm/src/main/java/org/iuedesarrollosoftware/orm/servlets/SrvDetalleAutor.java
@@ -0,0 +1,18 @@
+package org.iuedesarrollosoftware.orm.servlets;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "SrvDetalleAutor", value = "/SrvDetalleAutor")
+public class SrvDetalleAutor extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
